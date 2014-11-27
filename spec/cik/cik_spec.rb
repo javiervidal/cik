d6d1a00d56e7d3ef7c91df532449c318a4cdccdb
Javier Vidal <javier@javiervidal.net>
2014-11-27 09:34:02 +0100

    Adding pending tests

---
+++ spec/cik/cik_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Cik do
+
+  describe '.lookup' do
+
+    context 'when a valid symbol is looked up' do
+
+      it 'returns a cik'
+      it 'returns an entity name'
+      it 'returns a sic'
+
+    end
+
+    context 'when an invalid symbol is looked up' do
+
+      it 'returns nil'
+
+    end
+
+  end
+
+end
