7885ad9c9edfc61ffbefb33c9eec72bc4a7c33fa
Javier Vidal <javier@javiervidal.net>
2014-11-28 16:46:02 +0100

    Implementing tests without stubbing calls to www.sec.gov

--- lib/cik/cik.rb
+++ lib/cik/cik.rb
@@ -1,23 +1,38 @@
 class Cik
 
+  require 'open-uri'
   require 'nokogiri'
 
   class << self
 
     def lookup(symbol)
-      doc = Nokogiri::HTML(open("http://www.sec.gov/cgi-bin/browse-edgar?CIK=#{symbol}&action=getcompany"))
-      md = /(.+)\sCIK#:\s(\d{10})/.match(doc.css("div.companyInfo").css("span.companyName").text)
+      doc = Nokogiri::HTML(open(edgar_lookup_url(symbol)))
+      md = /(.+)\sCIK#:\s(\d{10})/.match(company_info(doc))
       if md
         {
           cik: md[2],
           name: md[1],
-          sic: doc.css("p.identInfo a")[0].text
+          sic: sic(doc)
         }
       else
         nil
       end
     end
 
+    def edgar_lookup_url(symbol)
+      "http://www.sec.gov/cgi-bin/browse-edgar?CIK=#{symbol}&action=getcompany"
+    end
+
+    def company_info(doc)
+      doc.css("div.companyInfo").css("span.companyName").text
+    end
+
+    def sic(doc)
+      doc.css("p.identInfo a")[0].text
+    end
+
   end
+
+  private_class_method :edgar_lookup_url, :edgar_lookup_url, :sic
 
 end
--- spec/cik/cik_spec.rb
+++ spec/cik/cik_spec.rb
@@ -6,15 +6,29 @@ describe Cik do
 
     context 'when a valid symbol is looked up' do
 
-      it 'returns a cik'
-      it 'returns an entity name'
-      it 'returns a sic'
+      let(:entity) { Cik.lookup('CSCO') }
+
+      it 'returns a cik' do
+        expect(entity[:cik]).to eq('0000858877')
+      end
+
+      it 'returns an entity name' do
+        expect(entity[:name]).to eq('CISCO SYSTEMS, INC.')
+      end
+
+      it 'returns a sic' do
+        expect(entity[:sic]).to eq('3576')
+      end
 
     end
 
     context 'when an invalid symbol is looked up' do
 
-      it 'returns nil'
+      let(:entity) { Cik.lookup('ZZZZ') }
+
+      it 'returns nil' do
+        expect(entity).to be_nil
+      end
 
     end
 
