d24fac6dd0827d4216a4e3f78f4d079209bbec3b
Javier Vidal <javier@javiervidal.net>
2014-12-01 07:45:18 +0100

    Updating SimpleCov.formatters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 require 'simplecov'
-SimpleCov.start
-
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+
+SimpleCov.start
 
 require 'cik'
 require 'webmock/rspec'
